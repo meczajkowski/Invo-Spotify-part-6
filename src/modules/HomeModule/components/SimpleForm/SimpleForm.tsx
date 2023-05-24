60f61dfdba701b16121349da024541c771ed9aad
Michal <michal.erik.czajkowski@gmail.com>
2023-05-24 15:09:53 +0200

    add: handling the form

--- src/modules/HomeModule/components/SimpleForm/SimpleForm.tsx
+++ src/modules/HomeModule/components/SimpleForm/SimpleForm.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SimpleForm.scss';
 
 export const SimpleForm: React.FC = () => {
+  interface IForm {
+    [key: string]: {
+      value: string;
+    };
+  }
+
+  const [form, setForm] = useState<IForm>({
+    fullName: {
+      value: '',
+    },
+    cardNumber: {
+      value: '',
+    },
+    expiryDate: {
+      value: '',
+    },
+    CVV: {
+      value: '',
+    },
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setForm({ ...form, [name]: { ...form[name], value } });
+  };
+
   return (
-    <form className='simple-form'>
+    <form className='simple-form' onSubmit={handleSubmit}>
       <h1 className='simple-form__title'>Add your informations</h1>
       <input
         className='simple-form__input'
         type='text'
         name='fullName'
         placeholder='Name on card'
+        onChange={handleFormChange}
       />
       <input
         className='simple-form__input'
         type='text'
         name='cardNumber'
         placeholder='Card number'
+        onChange={handleFormChange}
       />
       <div className='simple-form__flex-container'>
         <input
@@ -23,12 +58,14 @@ export const SimpleForm: React.FC = () => {
           type='text'
           name='expiryDate'
           placeholder='Expiry date (MM/YY)'
+          onChange={handleFormChange}
         />
         <input
           className='simple-form__input'
           type='text'
           name='CVV'
           placeholder='CVV'
+          onChange={handleFormChange}
         />
       </div>
       <button className='simple-form__button' type='submit'>
